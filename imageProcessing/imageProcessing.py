abbc5e5a6f337c892c355c79e58901484268bde8
Irene (XiangYi) Chen <xy29chen@edu.uwaterloo.ca>
2017-11-06 10:14:43 -0500

    resize, invert colour, get black pixels now functional

--- imageProcessing/imageProcessing.py
+++ imageProcessing/imageProcessing.py
@@ -1,11 +1,31 @@
+from __future__ import division
 import cv2
+import cv
+import numpy as np
 
-#TODO resize image such that it fits within number of motor steps
+
+#resize image such that it fits within number of motor steps
+im_original = cv2.imread('maybeagoose.jpg')
+y, x = im_original.shape[:2]
+print(x, y)
+
+if (x > y):
+	rescale = 200/x
+	print(x, rescale)
+else:
+	rescale = 200/y
+	print(y, rescale)
+
+im_resize = cv2.resize(im_original, (0,0), fx=rescale, fy=rescale, interpolation = cv2.INTER_AREA) #common interpolation for shrinking
+
+cv2.imwrite('goose1resized.png', im_resize)
+y, x = im_resize.shape[:2]
 
 #TODO adjust contrast
 
 
-im_colour = cv2.imread('image7.png')
+#loading images, cvtColor converts to grayscale
+im_colour = im_resize
 im_gray = cv2.cvtColor(im_colour, cv2.COLOR_BGR2GRAY)
 
 (thresh, im_bw) = cv2.threshold(im_gray, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
@@ -13,13 +33,16 @@ im_gray = cv2.cvtColor(im_colour, cv2.COLOR_BGR2GRAY)
 thresh = 127
 im_bw = cv2.threshold(im_gray, thresh, 255, cv2.THRESH_BINARY)[1]
 
-cv2.imwrite('image_bw7.png',im_bw)
+cv2.imwrite('goose1bw.png',im_bw)
+
+
 
 #edge detection:
 im_blurC = cv2.GaussianBlur(im_colour, (7,7),0) #blurs to soften edges, not really sure how effetive this is yet
 im_blurBW = cv2.GaussianBlur(im_bw, (7,7),0)
 
-im_outlineC = cv2.Canny(im_blurC, 100, 200)#for colour #function that processes and outputs an image, 100 300 is the ratio for acceptable edge gradation 
+im_outlineC = cv2.Canny(im_blurC, 100, 200) #for colour 
+#processes and outputs an image, 100 300 is the ratio for acceptable edge gradation 
 im_outlineBW = cv2.Canny(im_blurBW, 100, 200) #for black and white
 im_outlineG = cv2.Canny(im_gray, 100, 200)#
 #so that we can merge all three together to get better acuracy of the image
@@ -27,9 +50,26 @@ im_outlineG = cv2.Canny(im_gray, 100, 200)#
 im_outlineTemp = cv2.addWeighted(im_outlineBW,1,im_outlineC,1,0) #merges two photos together
 im_outline = cv2.addWeighted(im_outlineTemp,1,im_outlineG,1,0)#merges a third to it
 
-#cv2.imwrite('image_outline10.png',im_outlineC)
-#cv2.imwrite('image_outline11.png',im_outlineBW)
-cv2.imwrite('image_outlineYAY2.png',im_outline)
-#cv2.imwrite('image_outline12.png',im_outlineG)
+#invert colours:
+im_outline = cv2.bitwise_not(im_outline);
+cv2.imwrite('goose1outline.png',im_outline)
+
 
 #TODO detect pixels (000), output in file
+#coordinates = np.where(zeros == [255])
+
+xblacks = []
+yblacks = []
+
+f = open('coordinates.txt', 'w')
+
+for i in range(im_outline.shape[0]):
+	for j in range(im_outline.shape[1]):
+		if (im_outline[i,j] == 0):
+			xblacks = np.append(xblacks, i)
+			yblacks = np.append(xblacks, j)
+			print>>f, i, j
+			
+
+
+
