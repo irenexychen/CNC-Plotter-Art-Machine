a517193651d62ff80228f82834a7f98d65d005bb
Tina Gao <thgao@uwaterloo.ca>
2017-11-21 20:06:33 -0500

    fixed parsing through serial read string's function in imageDrawing.

--- src/imageDrawing/imageDrawing.ino
+++ src/imageDrawing/imageDrawing.ino
@@ -24,8 +24,8 @@ void penUp();
 void penDown();
 void drawX(int n);
 void drawY(int n);
-int blockingRead();
-int convertInputToInt();
+void blockingRead(int currentX[], int currentY[]);
+void convertSerialStrInputToInt(int currentX[], int currentY[]);
 
 void setup()
 {
@@ -39,50 +39,34 @@ void loop()
 {
   while (drawBool == true)
   {
-    int currentX[50];
-    int currentY[50];
+    int currentX[49];
+    int currentY[49];
     delay (3000);
 
-    int numCoords = blockingRead();
-    
-    //sets up position, move to first coord
-    Serial.print("@GetNext"); //anything that is a command shouldn't have a \n
-    Serial.flush();
-    
-    currentX[0] = blockingRead();
-    currentY[0] = blockingRead();
+    int numCoords = Serial.parseInt();
 
-    penUp();
-    drawX((int)currentX[0]);
-    drawY((int)currentY[0]);
-    penDown();
+    xold = 0;
+    yold = 0;
 
-    xold = currentX[0];
-    yold = currentY[0];
-    //clear current, fresh memory to use in loop
-    currentX[0] = 0;
-    currentY[0] = 0;
     int iters = 50;
-    for (int i = 1; i < numCoords; i++)
+    for (int i = 0; i < numCoords; i = i + 49)
+      /* Outer for-loop keeps track of number of coordinates to go through, which overlaps with inner for-loop
+             which reads in 49 coordinates at the same time. Hence to merge the for-loops, the i counter of the outer
+             for-loop must be iterated along with the inner for-loop.
+      */
     {
       Serial.print("@GetNext"); //gives order to imageProcessing.py
       Serial.flush();
 
       //python sends in next 49 coords
+      blockingRead(currentX, currentY);
       for (int k = 0; k < iters; k++)
       {
-        currentX[k] = blockingRead();//reads x
         if (currentX[k] == -1) //end of file (not 49 full coords), blocked read returns -1 instead of a coordinate
         {
           iters = k;
           break;
         }
-        currentY[k] = blockingRead(); //reads y
-        i++;
-        /* Outer for-loop keeps track of number of coordinates to go through, which overlaps with inner for-loop
-         * which reads in 49 coordinates at the same time. Hence to merge the for-loops, the i counter of the outer
-         * for-loop must be iterated along with the inner for-loop.
-         */
       }
 
       for (int k = 0; k < iters; k++)
@@ -115,23 +99,23 @@ void loop()
 void penUp()
 {
   penServo.write(70);//pulls pen up
-  delay(1000); 
+  delay(1000);
 }
 
 void penDown()
 {
   penServo.write(0);//pulls pen up
-  delay(1000); 
+  delay(1000);
 }
 
 void drawX(int stepsToMove)
 {
   int steps;
-  if (stepsToMove > 0) 
+  if (stepsToMove > 0)
   {
-     steps = 1;
+    steps = 1;
   }
-  else 
+  else
   {
     stepsToMove *= -1;
     steps = -1;
@@ -147,9 +131,9 @@ void drawX(int stepsToMove)
 void drawY(int stepsToMove)
 {
   int steps;
-  if (stepsToMove > 0) 
+  if (stepsToMove > 0)
   {
-     steps = 1;
+    steps = 1;
   }
   else
   {
@@ -163,38 +147,43 @@ void drawY(int stepsToMove)
   }
 }
 
-int blockingRead()
+void blockingRead(int currentX[], int currentY[] )
 {
   while (!Serial.available())
   {
     delay(100);
   }
-  return convertInputToInt();
+  convertSerialStrInputToInt(currentX, currentY);
 }
 
-int convertInputToInt()
+void convertSerialStrInputToInt(int currentX[], int currentY[])
 { //only called when serial.available
   String resultStr;
   int resultInt;
   String inputString = "";
-  int flag = 0;
-  
-  char tempChar = Serial.read();
-  if (tempChar == '!') //end of entire coordinates to be sent from python
+
+  char tempCharX;
+  char tempCharY;
+
+
+  for (int i = 0; i < 50; i++)
   {
-    flag = 1;
-    resultInt = -1;
-  }
-  if (flag != 1) //not end of file
-  {
-    while (tempChar != ' ' && tempChar != '\n')
+    tempCharX = Serial.read();
+    if (tempCharX == '!') //end of entire coordinates to be sent from python
     {
-      inputString += (char)tempChar;
-      tempChar = Serial.read();
+      resultInt = -1;
+      currentX[i] = resultInt;
+      break;
+    }
+
+    while (tempCharX != ' ') //python inputs coordinates in a string separated by spaces & on diff lines
+    {
+      inputString += (char)tempCharX;
+      tempCharX = Serial.read();
     }
     for (int i = 0; i < inputString.length(); i++)
     {
-      if (inputString[i] == '.')
+      if (inputString[i] == '.') //python puts integers as floats into input fmor serial.read(), we just want it as an int
       {
         break;
       }
@@ -204,6 +193,29 @@ int convertInputToInt()
       }
     }
     resultInt = resultStr.toInt();
+    resultStr = "";
+    currentX[i] = resultInt;
+    
+    tempCharY = Serial.read();
+    while (tempCharY != '\n')//python inputs coordinates in a string separated by spaces & on diff lines
+    {
+      inputString += (char)tempCharY;
+      tempCharY = Serial.read();
+    }
+    for (int i = 0; i < inputString.length(); i++)
+    {
+      if (inputString[i] == '.') //python puts integers as floats into input fmor serial.read(), we just want it as an int
+      {
+        break;
+      }
+      else if (inputString[i] >= 48 && inputString[i] <= 57) //is an integer 0 to 9
+      {
+        resultStr += inputString;
+      }
+    }
+    resultInt = resultStr.toInt();
+    resultStr = "";
+    currentY[i] = resultInt;
+
   }
-  return resultInt;
 }
